2e86cfa6aab2ec76255697d9664a76054c8b80f5
camila <camiramirezpp@gmail.com>
2018-09-20 21:38:42 -0300

    touchmove

--- docs/js/app.js
+++ docs/js/app.js
@@ -2,7 +2,7 @@
 $(document).ready(function() {
   var $magic = $(".magic"),
       magicWHalf = $magic.width() / 2;
-  $(document).on("mousemove", function(e) {
+  $(document).on("touchmove, mousemove", function(e) {
     $magic.css({"left": e.pageX - magicWHalf, "top": e.pageY - magicWHalf});
   });
 });
